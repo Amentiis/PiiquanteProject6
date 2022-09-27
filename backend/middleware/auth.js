b4745125032a567877004f8ce9f1e4f11c71b697
Amentiis <mat-leseur@hotmail.fr>
2022-09-27 14:58:03 +0200

    Last commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
-
-console.log(process.env.JWT_TOKEN_KEY);
 //On vérifie si le token de l'utilisateur corresponds bien au userid renseigné
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, "IMASECRETTOKEN");
+    const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_KEY);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
